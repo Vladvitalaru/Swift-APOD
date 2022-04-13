2b089c901821def68489d0d119c48817f0da38b8
Vlad <vladvitalaru@yahoo.com>
2022-04-13 15:22:21 -0700

    Initial Commit

---
+++ APOD/APODApp.swift
@@ -0,0 +1,17 @@
+//
+//  APODApp.swift
+//  APOD
+//
+//  Created by Vlad Vitalaru on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct APODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
